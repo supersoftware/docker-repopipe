58103d47ea4afc7694b27fc0ab18d49f1a1a7dce
yamazawa <yamazawa@tokyo.supersoftware.co.jp>
2017-07-27 19:01:58 +0900

    .

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-eval $(ssh-agent -s)
-ssh-add <(echo $SSH_PRIVATE_KEY)
 mkdir -p ~/.ssh
+echo $SSH_PRIVATE_KEY > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+eval $(ssh-agent -s)
+ssh-add
 
 git clone $ORIGIN_REPO repo
 cd repo
 git remote add upstream $UPSTREAM_REPO
 git push upstream master
 cd .. && rm -rf repo
+
+ssh-add -D

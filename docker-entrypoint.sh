622b001c0b0b30cbe2a7935867a5d97945e520bc
yamazawa <yamazawa@tokyo.supersoftware.co.jp>
2017-07-27 18:45:21 +0900

    .

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 eval $(ssh-agent -s)
-ssh-add <(echo "$SSH_PRIVATE_KEY")
+ssh-add <(echo $SSH_PRIVATE_KEY)
 mkdir -p ~/.ssh
 echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
 

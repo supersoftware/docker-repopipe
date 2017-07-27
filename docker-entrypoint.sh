976131fe7476cbeb271d5f16499a31e3aff3aae3
yamazawa <yamazawa@tokyo.supersoftware.co.jp>
2017-07-27 20:09:03 +0900

    .

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+ln -s $CA_CERT /usr/local/share/ca-certificates/ca.crt
+update-ca-certificates
+
 git clone $(cat $ORIGIN_REPO) repo
 cd repo
 git remote add upstream $(cat $UPSTREAM_REPO)
